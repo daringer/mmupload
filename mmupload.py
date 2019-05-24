1039c200466b1f95a7b97462855ea077c3a20821
Markus Meissner <coder@safemailbox.de>
2019-05-24 23:47:13 +0200

    fully recursive

--- mmupload.py
+++ mmupload.py
@@ -1,18 +1,102 @@
 import sys
 import os
 
-from flask import Flask, render_template, request, flash, redirect, Response
+from flask import Flask, render_template, request, flash, redirect, Response, url_for
 from werkzeug import secure_filename
 
 from flask import Blueprint, render_template
 main = Blueprint("main", __name__, template_folder="pages")
 
-from flask_uploads import UploadSet, configure_uploads, ALL
+#from flask_uploads import UploadSet, configure_uploads, ALL
 
 from urllib.parse import quote, urlparse
 
 import yaml
 import re
+
+class FileDBError(Exception): pass
+class InvalidName(FileDBError): pass
+class DirAlreadyExists(FileDBError): pass
+class DirNotExisting(FileDBError): pass
+class DirNotEmpty(FileDBError): pass
+class NotDeletingRootDir(FileDBError): pass
+class NotAFileError(FileDBError): pass
+
+class FileDB:
+
+    raw_name_pat = "^[a-zA-Z0-9-_]+$"
+    name_pat = re.compile(raw_name_pat)
+
+    def __init__(self, root_dir, rel_dir=""):
+        self.root_dir = root_dir
+        self.rel_dir = rel_dir
+        self.base_dir = root_dir
+
+        self.upload_sets_map = {}
+
+    def get_contents(self, dirname=""):
+        return os.listdir(os.path.join(self.base_dir, dirname))
+
+    def get_dirs(self, dirname):
+        return [p for p in self.get_contents(dirname)
+                if self.isdir(os.path.join(dirname, p))]
+
+    def get_files(self, dirname):
+        return [p for p in self.get_contents(dirname)
+                if self.isfile(os.path.join(dirname, p))]
+
+    def get_path(self, rel_path):
+        print (os.path.join(self.base_dir, rel_path))
+        return os.path.join(self.base_dir, rel_path)
+
+    def isdir(self, rel_path):
+        return os.path.isdir(self.get_path(rel_path))
+
+    def isfile(self, rel_path):
+        return os.path.isfile(self.get_path(rel_path))
+
+    def create_dir(self, dirname, new_dir):
+        if not self.name_pat.match(new_dir):
+            raise InvalidName(f"{name} (need: {self.raw_name_pat})")
+        if new_dir in self.get_dirs(dirname):
+            raise DirAlreadyExists(new_dir)
+        return os.makedirs(self.get_path(os.path.join(dirname, new_dir)))
+
+    def create_file(self, dirname, data):
+        """ gwpokgpwrgkprowpgoropkwgpo
+        poqrkgfwqpfekoqfe
+        feopfeqwpefoqwkpofeq
+        """
+
+        print (self.base_dir)
+        print (dir(data))
+        path = self.get_path(os.path.join(dirname, data.filename))
+        data.save(path)
+        return path
+
+
+        #data is more complex!
+        #print (data, dir(data))
+        #with open(data, "w") as fd:
+        #    fd.write(data)
+        #return self.upload_set.save(data)
+
+    def delete_dir(self, dirname):
+        if not self.isdir(dirname):
+            raise DirNotExisting(dirname)
+        if len(self.get_contents(dirname)) > 0:
+            raise DirNotEmpty(self.get_path(dirname))
+        if os.path.abspath(self.root_dir) == os.path.abspath(self.get_path(dirname)):
+            raise NotDeletingRootDir()
+        if self.get_path(dirname) in self.upload_sets_map:
+            del self.upload_sets_map[self.get_path(dirname)]
+        return os.rmdir(self.get_path(dirname))
+
+    def get_file(self, rel_path):
+        path = self.get_path(rel_path)
+        if not self.isfile(rel_path):
+          raise NotAFileError(path)
+        return open(path).read()
 
 # determine config file path
 config_path = f"/srv/flask/{os.getlogin()}/mmupload/mmupload.yaml"
@@ -28,109 +112,76 @@ cfg = yaml.load(open(config_path))
 
 app = Flask(__name__)
 app.secret_key = cfg["secret_key"]
-app.config["UPLOADS_DEFAULT_DEST"] = cfg["file_destination"]
+#app.config["UPLOADS_DEFAULT_DEST"] = cfg["file_destination"]
+
+#app.config["UPLOADS_my_DEST"] = cfg["file_destination"]
 app.register_blueprint(main, url_prefix="/")
 
-upload_sets = {}
-for cat_id in cfg["category"].keys():
-    #app.config[f"UPLOADS_{cat_id}_DEST"] = \
-    #  os.path.join(cfg["file_destination"], cat_id)
-
-    upload_sets[cat_id] = UploadSet(cat_id, ALL)
-
-configure_uploads(app, upload_sets.values())
+filedb = FileDB(cfg["file_destination"], "")
+#configure_uploads(app, (filedb.upload_set, ))
 
 
-@app.route("/", methods=["GET", "POST"])
-def upload():
-    cfg = yaml.load(open(config_path))
-    if request.method == "POST" and "target" in request.files:
-        cat_id = request.form.get("cat_id")
-        fset = upload_sets.get(cat_id)
-        filename = fset.save(request.files["target"])
-        flash(f"Saved to category: {cat_id}")
-        return redirect("/")
+@app.route("/up/<path:dirname>", methods=["POST"])
+def upload(dirname):
+    #dirname = request.form["dirname"]
 
+    if not "target" in request.files:
+      flash(repr(NotAFileError()))
+      return redirect(url_for("show", dirname=dirname))
+
+    app.config["UPLOADS_FILES_DEST"] = filedb.get_path(dirname)
+
+    filename = filedb.create_file(dirname, request.files["target"])
+    flash(f"Saved to: {filename}")
+    return redirect(url_for("show", dirname=dirname))
+
+@app.route("/")
+@app.route("/dir/")
+@app.route("/dir/<path:dirname>")
+def show(dirname=""):
+    #print (filedb.get_dirs(dirname))
+    #print (filedb.get_files(dirname))
+    parent = os.path.dirname(dirname)
     return render_template("tmpl.html",
-        show_categories=True, show_upload=True,
-        categories=cfg["category"].keys(), file_id=""
+        show_dirs=True, show_upload=True if dirname != "" else False, show_files=True,
+        dirs=map(lambda d: (d, os.path.join(dirname, d)), filedb.get_dirs(dirname)),
+        files=map(lambda f: (f, os.path.join(dirname, f)), filedb.get_files(dirname)),
+        parent_dir="" if dirname == "" else os.path.basename(parent),
+        parent_path="" if dirname == "" else parent,
+        base_dir=dirname if dirname != "" else ".",
+        base_dir_name=os.path.basename(dirname if dirname != "" else ".")
     )
 
-@app.route("/cat", methods=["POST"])
-def cat_new():
-    cfg = yaml.load(open(config_path))
-    if not "new_cat_name" in request.form:
-        flash("new category id not provided")
+@app.route("/new_dir/<path:dirname>", methods=["POST"])
+def create_dir(dirname):
+
+    if not "new_dirname" in request.form:
+        flash("new dirname not provided")
         return redirect("/")
 
-    cat_id = request.form.get("new_cat_name")
-    pat = re.compile("^[a-zA-Z0-9-_]+$")
-    if not pat.match(cat_id):
-        flash("only [a-zA-Z0-9-_] are allowed as category_id, no whitespace!")
-        return redirect("/")
+    new_dirname = request.form.get("new_dirname")
+    filedb.create_dir(dirname, new_dirname)
+    flash(f"directory created: {new_dirname}")
+    return redirect(url_for("show", dirname=dirname))
 
-    if len(cat_id) < 1:
-        flash("the category must have at least 3 chars...")
-        return redirect("/")
+@app.route("/del_dir/<path:dirname>", methods=["GET"])
+def delete_dir(dirname):
+    try:
+        filedb.delete_dir(dirname)
+        flash(f"deleted category: {dirname}")
+    except FileDBError as e:
+      flash(repr(e))
+    return redirect(url_for("show", dirname=os.path.dirname(dirname)))
 
-    if cat_id in cfg["category"]:
-        flash(f"cat with id: {cat_id} already exists, try another")
-        return redirect("/")
-
-    cfg.setdefault("category", {}).setdefault(cat_id, {});
-    yaml.dump(cfg, open(config_path, "w"))
-    upload_sets[cat_id] = UploadSet(cat_id, ALL)
-    configure_uploads(app, upload_sets.values())
-    return redirect("/")
-
-@app.route("/cat/<cat_id>/del", methods=["GET"])
-def cat_del(cat_id):
-    cfg = yaml.load(open(config_path))
-
-    if cat_id not in cfg["category"]:
-        flash(f"tried deletion of non-exising category: {cat_id}")
-        return redirect("/")
-
-    cat_dir = os.path.join(cfg["file_destination"], cat_id)
-    if not os.path.exists(cat_dir):
-        flash("category directory does not exist, bad but ok for deletion...")
-
-    elif len(os.listdir(cat_dir)) > 0:
-        flash(f"cannot delete cat_id: {cat_id} -> not empty")
-        return redirect("/")
-
-    del cfg["category"][cat_id]
-    del upload_sets[cat_id]
-    configure_uploads(app, upload_sets.values())
-    yaml.dump(cfg, open(config_path, "w"))
-    flash(f"deleted category: {cat_id}")
-    return redirect("/")
-
-@app.route("/cat/<cat_id>/show", methods=["GET"])
-def cat_show(cat_id):
-
-    cat_dir = os.path.join(cfg["file_destination"], cat_id)
-    if not os.path.exists(cat_dir):
-        flash(f"category: {cat_id} is empty...")
-        return redirect("/")
-
-    return render_template("tmpl.html",
-        show_upload=True, show_files=True,
-        categories=cfg["category"].keys(),
-        files=os.listdir(cat_dir), #[(file_id, quote(file_id)) for file_id in os.listdir(cat_dir)],
-        cat_id=cat_id
-    )
-
-@app.route("/cat/<cat_id>/get/<file_id>", methods=["GET"])
-def get_file(cat_id, file_id):
-    cat_dir = os.path.join(cfg["file_destination"], cat_id)
-    full_path = os.path.join(cat_dir, file_id)
-    content = open(full_path).read()
+@app.route("/get/<path:target>", methods=["GET"])
+def get_file(target):
+    content = filedb.get_file(target)
     return Response(content, mimetype="octet/stream")
 
-@app.route("/get/<file_id>", methods=["GET"])
-def get_pub_file(file_id):
-  pass
+
+#@app.route("/get/<file_id>", methods=["GET"])
+#def get_pub_file(file_id):
+#  pass
 
 
 #@app.route('/photo/<id>')
