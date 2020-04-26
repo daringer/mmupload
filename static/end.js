0e6d6912e82c5b5c6cf95f428288f4a3c4fbf88e
Markus Meissner <coder@safemailbox.de>
2020-04-26 17:20:39 +0200

    ace theme

--- static/end.js
+++ static/end.js
@@ -184,7 +184,8 @@ $(function() {
 
 	editor = ace.edit("editor");
 	//editor.setTheme("ace/theme/twilight");
-	editor.setTheme("ace/theme/terminal");
+	//editor.setTheme("ace/theme/terminal");
+	editor.setTheme("ace/theme/IPlastic");
 	//editor.session.setMode("ace/mode/javascript");
 
 	editor.on("change", function(ev) {
